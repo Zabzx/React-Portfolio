26e417efb201ec8aad01a3b0240038ccbe1a66e1
Ziabeher Phillips <zabq02@gmail.com>
2022-03-07 17:00:39 -0400

    experience section completed

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,140 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>My Skills</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__development">
+          <h3>Development Laguages / Frameworks</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>JavaScript</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>Tailwind CSS</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>React JS</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>Elixir</h4>
+              <small className="text-light">Beginner</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>C#</h4>
+              <small className="text-light">Beginner</small>
+              </div>
+            </article>
+          </div>
+        </div>
+         {/* Tools */}
+
+        <div className="experience__tools">
+        <div className="experience__development">
+          <h3>Development Tools / Services</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>Git</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>GitHub</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>Netlify</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>Figma</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>Adobe XD</h4>
+              <small className="text-light">Beginner</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>NPM</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <div>
+              <h4>Ubuntu</h4>
+              <small className="text-light">Beginner</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
