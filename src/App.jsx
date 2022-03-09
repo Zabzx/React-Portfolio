a5d2059f8f85b3dd49b3323bcece0b91882352f6
Ziabeher Phillips <zabq02@gmail.com>
2022-03-08 22:05:46 -0400

    contact page done

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ const App = () => {
     <About/>
     <Experience/>
     <Portfolio/>
-    <Testemonials/>
     <Contact/>
     <Footer/>
     </>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './contact.css'
+import {HiOutlineMail} from 'react-icons/hi'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_xovc2r4', 'template_bwma5z6', form.current, '3p-TKLixBJNxY_hBJ')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      
+      e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <HiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>ziabeherphillips@gmail.com</h5>
+            <a href="mailto:ziabeherphillips@gmail.com">Send a message</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder="Your Full Name" required />
+          <input type="email" name='email' placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
