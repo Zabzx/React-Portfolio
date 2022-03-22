9e625ebab56a1c19f9539ba2ba1c7a845f31abf2
Ziabeher Phillips <zabq02@gmail.com>
2022-03-22 12:05:17 -0400

    finalized personalization

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ import './header.css'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} className="btn" download >Download CV</a>
+        <a href={CV} className="btn" download >Download Resume</a>
         <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
   )
